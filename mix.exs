33a8dfdf3a4a498cf0fd9bb0f2656bc621b874b2
Steven Barber <barberappdev@gmail.com>
2026-09-14 22:11:21 -0700

    chore(): prepare 0.1.0-beta.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Canopy.MixProject do
   def project do
     [
       app: :canopy,
-      version: "0.1.0",
+      version: "0.1.0-beta.1",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
